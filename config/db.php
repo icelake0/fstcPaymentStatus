53099ab7732a82f64e361ff94c8514287c75f565
icelake0 <ajiboyeag@gmail.com>
2017-10-21 09:55:17 +0100

    commiting for sam restore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=sql308.website.tk;dbname=wetk_20758696_test',
-    'username' => 'wetk_20758696',
-    'password' => '1swordfish',
-    'charset' => 'utf8',
-];
